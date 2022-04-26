de23e575e9ff8fc1016b1166c49d06130dc49252
ktw33 <kevinwright@MacBook-Pro.lan>
2022-04-26 12:35:28 -0400

    testing

---
+++ src/components/canvas.svelte
@@ -0,0 +1,39 @@
+<div id="test" ondrop="drop(event)" ondragover="allowDrop(event)">
+
+<h1 draggable="true" ondragstart="drag(event)" id="canvas">Canvas</h1> 
+
+</div>
+
+<div id="testTarget" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+
+
+<style>
+#test {
+    border: 2px solid black;
+    width: 100%;
+    height: 200px;
+}
+#testTarget {
+    border: 2px solid black;
+    width: 100%;
+    height: 200px;
+}
+
+</style>
+
+<script>
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+</script>
